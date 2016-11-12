71e9861f05b7873a582cf514b0775b31ac4529e0
nebulon42 <nebulon42@yandex.com>
2016-11-12 14:15:39 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'at-address-compare',
   packages = ['ataddrcompare'],
-  version = '0.1.0',
+  version = '0.1.1',
   entry_points = {
     'console_scripts': ['ataddrcompare = ataddrcompare.ataddrcompare:main']
   },
@@ -11,7 +11,7 @@ setup(
   description = 'Address comparison of OSM data with official open data for Austria.',
   author = 'Michael Glanznig',
   url = 'https://github.com/gmgeo/at-address-compare',
-  download_url = 'https://github.com/gmgeo/at-address-compare/tarball/0.1.0',
+  download_url = 'https://github.com/gmgeo/at-address-compare/tarball/0.1.1',
   keywords = ['Austria', 'OSM', 'OpenStreetMap', 'OpenData', 'addresses', 'address', 'compare'],
   license = 'MIT',
   classifiers = [],
