2d3d609c4bd4a42e6b2e58a8f1f8eb2f3befa18f
nebulon42 <nebulon42@yandex.com>
2016-11-12 12:25:28 +0100

    change version info to semver

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   description = 'Address comparison of OSM data with official open data for Austria.',
   author = 'Michael Glanznig',
   url = 'https://github.com/gmgeo/at-address-compare',
-  download_url = 'https://github.com/gmgeo/at-address-compare/tarball/0.1',
+  download_url = 'https://github.com/gmgeo/at-address-compare/tarball/0.1.0',
   keywords = ['Austria', 'OSM', 'OpenStreetMap', 'OpenData', 'addresses', 'address', 'compare'],
   license = 'MIT',
   classifiers = [],
